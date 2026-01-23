817e286ae5a823a0d2d888ba50e1753051672c60
playzayahya-maker <playzayahya@gmail.com>
2026-01-23 18:23:37 +0100

    Add files via upload
    First version of my secure portfolio

---
+++ script.js
@@ -0,0 +1,38 @@
+const CONFIG = { ADMIN_KEY: "SHENA_ADMIN" };
+
+function unlockVault() {
+    const input = document.getElementById('passkey');
+    const log = document.getElementById('error-log');
+    const val = input.value.trim();
+
+    // Security check for SQLi
+    if (/[';\-\-]/.test(val) || /OR /i.test(val)) {
+        log.style.color = "#ff3e3e";
+        log.innerText = ">> SECURITY_PROTOCOL_BLOCK: INJECTION_ATTEMPT";
+        return;
+    }
+
+    if (val === CONFIG.ADMIN_KEY) {
+        document.getElementById('gate-page').style.display = 'none';
+        const portal = document.getElementById('portal-animation');
+        portal.classList.add('active');
+        portal.style.opacity = "1";
+
+        setTimeout(() => {
+            portal.classList.remove('active');
+            portal.style.display = 'none';
+            document.getElementById('cv-page').style.display = 'flex';
+        }, 2000);
+    } else {
+        log.style.color = "#ff3e3e";
+        log.innerText = ">> ACCESS_DENIED: INVALID_KEY";
+    }
+}
+
+function lockVault() { location.reload(); }
+
+document.addEventListener('keypress', (e) => {
+    if (e.key === 'Enter' && document.getElementById('gate-page').style.display !== 'none') {
+        unlockVault();
+    }
+});
